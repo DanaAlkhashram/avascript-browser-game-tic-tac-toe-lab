0bb455301cb997685e91883e92c990f7c0b256f6
Dana <danalkhashram@gmail.com>
2025-06-29 18:24:13 +0300

    part 4-5 done

--- js/app.js
+++ js/app.js
@@ -15,27 +15,69 @@
 
 
 /*-------------------------------- Constants --------------------------------*/
-
+const values = ['X','0',''];
+const index = ['0','1','2','3','4','5','6','7','8'];
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
 
 /*---------------------------- Variables (state) ----------------------------*/
-let board = ['','','','','','','','',''];
-let turn = 'X';
-let winner = false;
-let tie = false;
+let board ;
+let turn ;
+let winner ;
+let tie ;
 
 /*------------------------ Cached Element References ------------------------*/
-squreEl = document.querySelectorAll(".sqr");
-messageEl = document.querySelector("#message");
- console.log(squreEl)
+const squreEls =document.querySelectorAll(".sqr");
+const messageEl =document.querySelector("#message");
+
 
 /*-------------------------------- Functions --------------------------------*/
-function init(){
 
+function init() {
+    board = ['X','0','','','','','','',''];
+    turn = 'X';
+    winner = false;
+    tie = false;
 
+    render()
 }
 
 init()
 
+function render(){
+    updateBoard();
+    updateMessage();
+
+}
+
+function updateBoard(){
+    board.forEach((value,index) => {
+        let square = squreEls[index];
+
+        if ( value === 'X' || value === '0' || value === '' )
+        square.textContent = value;
+ 
+    })
+}
+
+function updateMessage(){
+    if( winner === false && tie === false ){
+        messageEl.textContent =`It's ${turn}'s turn`
+    } else if ( winner === false && tie === true ) {
+        messageEl.textContent = `It iss a tie!`;
+    } else if ( winner === true ){
+        messageEl.textContent = `Congrats, ${turn} wins!`;
+    }
+
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
 
